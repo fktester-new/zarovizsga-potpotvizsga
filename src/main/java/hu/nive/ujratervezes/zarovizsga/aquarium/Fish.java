4c83c8580182897fdb997f2a369ef03279da38bd
fktester <kofe16@gmail.com>
2021-03-28 18:10:47 +0200

    Fish

---
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/aquarium/Fish.java
@@ -0,0 +1,50 @@
+package hu.nive.ujratervezes.zarovizsga.aquarium;
+
+public abstract class Fish {
+
+    private String name;
+    private int weight;
+    private String color;
+    private boolean memoryLoss = false;
+
+
+    public Fish(String name, int weight, String color) {
+        this.name = name;
+        this.weight = weight;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public abstract void feed();
+
+    public String status(Fish fish){
+        return fish.toString();
+    }
+
+    public boolean hasMemoryLoss(){
+        return memoryLoss;
+    }
+
+    protected void increaseWeight(int value){
+        weight += value;
+    }
+    protected void setMemoryLoss(boolean b){
+        memoryLoss = b;
+    }
+
+    @Override
+    public String toString() {
+        return name + ", weight: " + weight + ", color: " + color + ", short-term-memory-loss: " + memoryLoss;
+    }
+}
---
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/aquarium/Tang.java
@@ -0,0 +1,14 @@
+package hu.nive.ujratervezes.zarovizsga.aquarium;
+
+public class Tang extends Fish {
+
+    public Tang(String name, int weight, String color) {
+        super(name, weight, color);
+        setMemoryLoss(true);
+    }
+
+    @Override
+    public void feed() {
+        increaseWeight(1);
+    }
+}
